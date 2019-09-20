40710f117d2fd36f7381dddfdcdf44227b9fe022
Vikas Garg <vikas04522@gmail.com>
2019-09-19 22:30:54 -0400

    Added project structure

---
+++ src/screens/checkout/Checkout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Checkout.css';
+
+class Checkout extends Component {
+    render() {
+        return (
+            <div>
+                Checkout page
+            </div>
+        )
+    }
+}
+
+export default Checkout;
