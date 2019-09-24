6f42e4b4e3c32a6879d6369fdfd93d248312f751
Vikas Garg <vikas04522@gmail.com>
2019-09-23 23:56:12 -0400

    updated home page styles and removed the error in Home.js

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './Home.css';
 import Header from '../../common/Header';
 import GridList from '@material-ui/core/GridList';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardActionArea from '@material-ui/core/CardActionArea';
+import Card from '@material-ui/core/Card';
+import GridListTile from '@material-ui/core/GridListTile';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Grid } from '@material-ui/core';
+import { Icon } from '@iconify/react';
+import inrIcon from '@iconify/icons-fa/inr';
+import starO from '@iconify/icons-fa/star-o';
+import { isWidthUp } from '@material-ui/core/withWidth';
+
 
 class Home extends Component {
 
@@ -16,94 +19,139 @@ class Home extends Component {
         super();
         this.state = {
             restaurants: [],
-            restaurant_name = '',
-            categories = '',
-            customer_rating = 0,
-            average_price = 0
+            cards: 1,
         }
-        this.state.sortByCustomerRatingAsc = this.sortByCustomerRatingAsc.bind(this);
     }
-
-    sortByCustomerRatingAsc() {
-        this.setState(prevState => {
-          this.state.restaurants.sort((a, b) => (a.customer_rating - b.customer_rating))
-      });
-      }
-
-    // componentDidMount() {
-    //     fetch('http://localhost:8080/api/swagger-ui.html#!/restaurant-controller/getAllRestaurantsUsingGET')
-    //         .then(res => res.json())
-    //         .then((data) => {
-    //             this.setState({ restaurants: data })
-    //             console.log(this.state.restaurants)
-    //         })
-    //         .catch(console.log)
-    // }
 
     UNSAFE_componentWillMount() {
-        let data = null;
-        let xhr = new XMLHttpRequest();
+        sessionStorage.removeItem('customer-cart');
+
         let that = this;
-        xhr.addEventListener("readystatechange", function () {
+        let dataRestaurants = null;
+        let xhrRestaurants = new XMLHttpRequest();
+        xhrRestaurants.addEventListener('readystatechange', function () {
             if (this.readyState === 4) {
-                // console.log(JSON.parse(this.responseText));
-                this.setState({ restaurants: JSON.parse(this.responseText) });
+                that.setState({
+                    restaurants: JSON.parse(this.responseText).restaurants
+                })
             }
         })
+        xhrRestaurants.open('GET', `${this.props.baseUrl}restaurant`);
+        xhrRestaurants.send(dataRestaurants);
 
-        xhr.open("GET", this.props.baseUrl);
-        xhr.setRequestHeader("Cache-Control", "no-cache");
-        xhr.send(data);
+        this.updateCardsGridListCols();
     }
 
-    /* When clicked directs the user to the details page */
-    cardClickHandler = (e) => {
-        this.setState({ restaurants: <details /> }); /* direct to details page */
+    componentDidMount() {
+        window.addEventListener('resize', this.updateCardsGridListCols);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateCardsGridListCols);
+    }
+
+    updateCardsGridListCols = () => {
+        if (isWidthUp('xl', this.props.width)) {
+            this.setState({ cards: 6 });
+            return;
+        }
+
+        if (isWidthUp('lg', this.props.width)) {
+            this.setState({ cards: 5 });
+            return;
+        }
+
+        if (isWidthUp('md', this.props.width)) {
+            this.setState({ cards: 4 });
+            return;
+        }
+        if (isWidthUp('sm', this.props.width)) {
+            this.setState({ cards: 2 });
+            return;
+        }
+        this.setState({ cards: 1 });
+        return;
+    }
+
+    restaurantCardTileOnClickHandler = (restaurantId) => {
+        this.props.history.push('/restaurant/' + restaurantId);
+    }
+
+    searchHandler = (event) => {
+        let that = this;
+        let dataRestaurants = null;
+        let xhrRestaurants = new XMLHttpRequest();
+        xhrRestaurants.addEventListener('readystatechange', function () {
+            if (this.readyState === 4) {
+                if (!JSON.parse(this.responseText).restaurants) {
+                    that.setState({
+                        restaurants: null,
+                    })
+                } else {
+                    that.setState({
+                        restaurants: JSON.parse(this.responseText).restaurants,
+                    })
+                }
+            }
+        })
+        if (event.target.value === '') {
+            xhrRestaurants.open('GET', `${this.props.baseUrl}restaurant`);
+        } else {
+            xhrRestaurants.open('GET', `${this.props.baseUrl}restaurant/name/${event.target.value}`);
+        }
+        xhrRestaurants.send(dataRestaurants);
     }
 
     render() {
-        const { classes } = this.props;
-        sortByCustomerRatingAsc = this.sortByCustomerRatingAsc;
-
         return (
             <div>
                 <Header />
-                <div className='root'>
-                    <GridList className='gridList' cols={4}>
-
-                        {this.state.restaurants.map((restaurants => (
-                            <Card className='card'>
-                                <CardActionArea onClick={this.cardClickHandler}>
+                {this.state.restaurants === null ?
+                    <Typography className='noRestaurant' variant='h6'>
+                        No restaurant with the given name.
+                    </Typography>
+                    :
+                    <GridList
+                        className='gridList'
+                        cols={this.state.cards}
+                        cellHeight='auto'
+                    >
+                        {this.state.restaurants.map(restaurant => (
+                            <GridListTile
+                                onClick={() => this.restaurantCardTileOnClickHandler(restaurant.id)}
+                                key={'restaurant' + restaurant.id}
+                            >
+                                <Card className='card' style={{ textDecoration: 'none' }}>
                                     <CardMedia
-                                        className='media'
-                                        image={this.state.restaurants.photo_url}
-                                        title="Restaurant Name"
+                                        className='restaurantCardMedia'
+                                        image={restaurant.photo_URL}
+                                        title={restaurant.restaurant_name}
                                     />
                                     <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            {this.state.restaurants.restaurant_name}
+                                        <Typography className='name' gutterBottom variant='h5' component='h2'>
+                                            {restaurant.restaurant_name}
                                         </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                            {this.state.restaurants.categories}
+                                        <Typography variant='subtitle1'>
+                                            {restaurant.categories}
                                         </Typography>
-                                        <Grid container spacing={2}>
-                                            <Grid item xs className='rating'>
-                                                <FontAwesomeIcon icon="star" />
-                                                {this.state.restaurants.customer_rating}
-                                            </Grid>
-                                            <Grid item xs className='avgPrice'>
-                                                <FontAwesomeIcon icon='inr' />
-                                                {this.state.restaurants.average_price}
-                                            </Grid>
-                                        </Grid>
+                                        <div className='ratingAvgRateDiv'>
+                                            <div className='ratingDiv'>
+                                                <Typography className='ratingText' variant='body2'>
+                                                    <Icon icon={starO} /> {restaurant.customer_rating} ({restaurant.number_customers_rated})
+                                                </Typography>
+                                            </div>
+                                            <Typography className='avgRateText' variant='body2'>
+                                                <Icon icon={inrIcon} /> {restaurant.average_price} for two
+                                            </Typography>
+                                        </div>
                                     </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        )))}
+                                </Card>
+                            </GridListTile>
+                        ))}
                     </GridList>
-                </div>
+                }
             </div>
-        )
+        );
     }
 }
 
