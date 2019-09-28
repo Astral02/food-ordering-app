5fa12bf74e4f85a4a067d094d450de1b1c8cab3e
Vikas Garg <vikas04522@gmail.com>
2019-09-28 05:02:34 -0400

    Used the font-awsome to get the icons and fixed css in detailed page for small screens

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Details.css';
 import Header from '../../common/Header';
-
+import 'font-awesome/css/font-awesome.min.css';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Card from '@material-ui/core/Card';
@@ -22,8 +22,7 @@ const styles = {
   },
   bullet: {
     display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    margin: '0 2px'
   },
   title: {
     fontSize: 14,
@@ -50,6 +49,7 @@ class Details extends React.Component {
       cartItemsList: [],
       snackbarMessage: null,
       cartTotalPrice: 0,
+      restaurant_id: null
     }
   };
 
@@ -66,16 +66,16 @@ class Details extends React.Component {
   };
 
   getRestaurantDetails = (id) => {
-    let that = this;
-    let url = `${this.props.baseUrl}${id}`;
+    let _this = this;
+    let url = `${this.props.baseUrl}restaurant/${id}`;
     return fetch(url, {
-      method:'GET',
+      method: 'GET',
     }).then((response) => {
       if (response.ok) {
         return response.json();
       }
     }).then((responseJson) => {
-      that.setState({
+      _this.setState({
         restaurant_name: responseJson.restaurant_name,
         photo_URL: responseJson.photo_URL,
         customer_rating: responseJson.customer_rating,
@@ -83,6 +83,8 @@ class Details extends React.Component {
         number_customers_rated: responseJson.number_customers_rated,
         locality: responseJson.address.locality,
         categories: responseJson.categories,
+        cartItems: 0,
+        restaurant_id: id,
       });
     }).catch((error) => {
       console.log('error getting data', error);
@@ -99,12 +101,35 @@ class Details extends React.Component {
   checkoutHandler = () => {
     if (this.state.cartItems === 0) {
       this.handleSnackBar("Please add an item to your cart!");
-    } else if (!sessionStorage.getItem("loggedIn")) {
+    } else if (!sessionStorage.getItem("access-token")) {
       this.handleSnackBar("Please login first!");
-    }else {
-      this.props.addItems(this.state.cartItemsList)
-      this.props.setCartTotal(this.state.cartTotalPrice)
-      this.props.history.push('/checkout');
+    } else {
+      //this.props.setData({ cartItemsList : this.state.cartItemsList});
+      //this.props.setCartTotal(this.state.cartTotalPrice)
+      //this.props.history.push('/checkout');
+
+      let customerCart = {
+        restaurantDetails: {
+          // 'address': this.state.address,
+          'average_price': this.state.average_price,
+          'categories': this.state.categories,
+          'customer_rating': this.state.customer_rating,
+          'id': this.state.restaurant_id,
+          'number_customers_rated': this.state.number_customers_rated,
+          'photo_URL': this.state.photo_URL,
+          'restaurant_name': this.state.restaurant_name,
+        },
+        cartItems: this.state.cartItemsList,
+        totalPrice: this.state.cartTotalPrice
+      };
+      sessionStorage.setItem('customer-cart', JSON.stringify(customerCart));
+      //this.props.setData({ 'customer-cart': this.state.cartItemsList });
+      this.props.history.push({
+        pathname: '/checkout',
+        data: customerCart,
+      });
+
+
     }
   }
 
@@ -161,116 +186,116 @@ class Details extends React.Component {
     });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
 
-      {/* Header */}
-      <div style={{marginTop: '64px'}}>
-        <Header showSearch={false} />
-      </div>
-
-      {/* restaurant information section */}
-      <div className="restaurant-information">
-        <div className="restaurant-image">
-          <div>
-            <img
-              src={this.state.photo_URL}
-              alt='restaurant'
-              width='100%'
-            />
-          </div>
+        {/* Header */}
+        <div >
+          <Header showSearch={false} />
         </div>
-        <div className="restaurant-details">
-          <div>
-            <Typography variant="h3" gutterBottom> {this.state.restaurant_name} </Typography> <br />
-            <Typography variant="h5" gutterBottom> {this.state.locality} </Typography> <br />
-            <Typography variant="body1" gutterBottom> {this.state.categories
-              && Array.isArray(this.state.categories)
-              && this.state.categories.length > 0
-              && this.state.categories.map((el) => el.category_name).join(", ")} </Typography>
-          </div>
-          <div style={{float:'left', display:"flex", flexDirection:"row", width:"100%", paddingTop:"5%"}}>
-            <div style={{width:"100%"}}>
-            <i className="fa fa-star" aria-hidden="true"> {this.state.customer_rating} </i>
-            <Typography variant="caption" gutterBottom> AVERAGE RATING BY <br /> <span style={{fontWeight: 'bold'}}> {this.state.number_customers_rated} </span> USERS </Typography>
-            </div>
-            <div style={{width:"100%"}}>
-            <i className="fa fa-inr" aria-hidden="true"> {this.state.average_price} </i>
-            <Typography variant="caption" gutterBottom> AVERAGE COST FOR <br /> TWO PEOPLE </Typography>
+
+        {/* restaurant information section */}
+        <div className="restaurant-information">
+          <div className="restaurant-image">
+            <div>
+              <img
+                src={this.state.photo_URL}
+                alt='restaurant'
+                width='100%'
+              />
             </div>
           </div>
-        </div>
-      </div>
-
-      <div className="menu-cart-section">
-
-        {/* menu-items section */}
-        <div className='menu'>
-          <div style={{padding:'3%'}}>
-            {this.state.categories.map(categoryItem =>
-              <div key={categoryItem.id}>
-                <CategoryItem item={categoryItem} this={this} />
+          <div className="restaurant-details">
+            <div>
+              <Typography variant="h4" gutterBottom> {this.state.restaurant_name} </Typography> <br />
+              <Typography variant="h5" gutterBottom> {this.state.locality} </Typography> <br />
+              <Typography variant="body1" gutterBottom> {this.state.categories
+                && Array.isArray(this.state.categories)
+                && this.state.categories.length > 0
+                && this.state.categories.map((el) => el.category_name).join(", ")} </Typography>
+            </div>
+            <div style={{ float: 'left', display: "flex", flexDirection: "row", width: "100%", paddingTop: "5%" }}>
+              <div style={{ width: "100%" }}>
+                <i className="fa fa-star" aria-hidden="true"> {this.state.customer_rating} </i> <br />
+                <Typography variant="caption" gutterBottom> AVERAGE RATING BY <br /> <span style={{ fontWeight: 'bold' }}> {this.state.number_customers_rated} </span> USERS </Typography>
               </div>
-            )}
+              <div style={{ width: "100%" }}>
+                <i className="fa fa-inr" aria-hidden="true"> {this.state.average_price} </i> <br />
+                <Typography variant="caption" gutterBottom> AVERAGE COST FOR <br /> TWO PEOPLE </Typography>
+              </div>
+            </div>
           </div>
         </div>
 
-        {/* my-cart section */}
-        <div className="cart">
-          <div style={{padding:'3%'}}>
-            <Card className={styles.card}>
-              <CardContent>
-                <div style={{display:"inline-block", width:"100%"}}>
-                  <div style={{float:"left", width:"10%"}}><Badge badgeContent={this.state.cartItems} color="primary"><ShoppingCart /></Badge></div>
-                  <div style={{float:"right", width:"90%"}}><Typography variant="h5" gutterBottom style={{fontWeight:'bold'}}> My Cart </Typography></div>
-                </div>
+        <div className="menu-cart-section">
 
-                {/* items in cart */}
-                {this.state.cartItemsList.map(cartItem =>
-                  <div key={cartItem.id}>
-                    <CartItem item={cartItem} this={this} />
+          {/* menu-items section */}
+          <div className='menu'>
+            <div style={{ padding: '3%' }}>
+              {this.state.categories.map(categoryItem =>
+                <div key={categoryItem.id}>
+                  <CategoryItem item={categoryItem} this={this} />
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* my-cart section */}
+          <div className="cart">
+            <div style={{ padding: '3%' }}>
+              <Card className={styles.card}>
+                <CardContent>
+                  <div style={{ display: "inline-block", width: "100%" }}>
+                    <div style={{ float: "left", width: "10%" }}><Badge badgeContent={this.state.cartItems === null ? 0 : this.state.cartItems} color="primary"><ShoppingCart /></Badge></div>
+                    <div style={{ float: "right", width: "90%" }}><Typography variant="h5" gutterBottom style={{ fontWeight: 'bold' }}> My Cart </Typography></div>
                   </div>
-                )}
 
-                <div style={{display:"inline-block", width:"100%", paddingTop:"3%"}}>
-                  <div style={{float:"left"}}><Typography variant="body1" gutterBottom style={{fontWeight:'bold'}}> TOTAL AMOUNT </Typography></div>
-                  <div style={{float:"right", width: "14%"}}><i className="fa fa-inr" aria-hidden="true"> {this.state.cartTotalPrice.toFixed(2)} </i></div>
-                </div>
-              </CardContent>
-              <CardActions>
-                <div style={{width:"100%"}}>
-                  <Button style={{width:"100%"}} variant="contained" color="primary" onClick={this.checkoutHandler}> CHECKOUT </Button>
-                </div>
-              </CardActions>
-            </Card>
+                  {/* items in cart */}
+                  {this.state.cartItemsList.map(cartItem =>
+                    <div key={cartItem.id}>
+                      <CartItem item={cartItem} this={this} />
+                    </div>
+                  )}
+
+                  <div style={{ display: "inline-block", width: "100%", paddingTop: "3%" }}>
+                    <div style={{ float: "left" }}><Typography variant="body1" gutterBottom style={{ fontWeight: 'bold' }}> TOTAL AMOUNT </Typography></div>
+                    <div style={{ float: "right", width: "14%" }}><i className="fa fa-inr" aria-hidden="true"> </i> {this.state.cartTotalPrice.toFixed(2)} </div>
+                  </div>
+                </CardContent>
+                <CardActions>
+                  <div style={{ width: "100%" }}>
+                    <Button style={{ width: "100%" }} variant="contained" color="primary" onClick={this.checkoutHandler}> CHECKOUT </Button>
+                  </div>
+                </CardActions>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
 
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        open={this.state.snackbarOpen}
-        autoHideDuration={1000}
-        onClose={(e) => this.handleSnackBar("")}
-        ContentProps={{
-          'aria-describedby': 'message-id',
-        }}
-        message={<span id="message-id">{this.state.snackbarMessage}</span>}
-        action={[
-          <IconButton
-            key="close"
-            aria-label="Close"
-            color="inherit"
-            onClick={(e) => this.handleSnackBar("")}
-          >
-          <CloseIcon />
-          </IconButton>,
-        ]}
-      />
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={this.state.snackbarOpen}
+          autoHideDuration={1000}
+          onClose={(e) => this.handleSnackBar("")}
+          ContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          message={<span id="message-id">{this.state.snackbarMessage}</span>}
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              onClick={(e) => this.handleSnackBar("")}
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
 
       </div>
     );
@@ -280,28 +305,28 @@ class Details extends React.Component {
 function CartItem(props) {
   const cartItem = props.item;
   const color = props.item
-  && props.item.item.item_type&&props.item.item.item_type.toString()
-  && props.item.item.item_type.toLowerCase() === "non_veg" ? "red" : "green";
+    && props.item.item.item_type && props.item.item.item_type.toString()
+    && props.item.item.item_type.toLowerCase() === "non_veg" ? "red" : "green";
   return (
-    <div style={{display:"flex", flexDirection:"row", width:"100%", padding:"1%"}}>
-      <div style={{width:"10%", display:"flex", alignItems:"center",color: color}}><i className="fa fa-stop-circle-o" aria-hidden="true"></i></div>
-      <div style={{width:"40%", display:"flex", alignItems:"center", textTransform:"capitalize"}}><span style={{color:"grey"}}> {cartItem.item.item_name} </span></div>
-      <div style={{width:"5%", display:"flex", alignItems:"center"}}>
+    <div style={{ display: "flex", flexDirection: "row", width: "100%", padding: "1%" }}>
+      <div style={{ width: "10%", display: "flex", alignItems: "center", color: color }}><i className="fa fa-stop-circle-o" aria-hidden="true"></i></div>
+      <div style={{ width: "40%", display: "flex", alignItems: "center", textTransform: "capitalize" }}><span style={{ color: "grey" }}> {cartItem.item.item_name} </span></div>
+      <div style={{ width: "5%", display: "flex", alignItems: "center" }}>
         <i onClick={(e) => props.this.removeItemFromCartHandler(cartItem)} className="cartButton fa fa-minus" aria-hidden="true" on></i>
       </div>
-      <div style={{width:"5%", display:"flex", alignItems:"center"}}> {cartItem.quantity} </div>
-      <div style={{width:"25%", display:"flex", alignItems:"center"}}>
+      <div style={{ width: "5%", display: "flex", alignItems: "center" }}> {cartItem.quantity} </div>
+      <div style={{ width: "25%", display: "flex", alignItems: "center" }}>
         <i onClick={(e) => props.this.addItemFromCartHandler(cartItem)} className="cartButton fa fa-plus" aria-hidden="true" on></i>
       </div>
-      <div style={{display:"flex", alignItems:"center"}}><i className="fa fa-inr" aria-hidden="true"><span style={{color:"grey"}}> {cartItem.item.price.toFixed(2)} </span></i></div>
+      <div style={{ display: "flex", alignItems: "center" }}><i className="fa fa-inr" aria-hidden="true"><span style={{ color: "grey" }}> {cartItem.item.price.toFixed(2)} </span></i></div>
     </div>
   )
 }
 
 function CategoryItem(props) {
   return (
-    <div style={{padding:"3%"}}>
-      <Typography variant="caption" gutterBottom style={{fontWeight:"bold", textTransform:"uppercase"}}> {props.item.category_name} </Typography>
+    <div style={{ padding: "3%" }}>
+      <Typography variant="caption" gutterBottom style={{ fontWeight: "bold", textTransform: "uppercase" }}> {props.item.category_name} </Typography>
       <Divider />
       {props
         && props.item
@@ -309,10 +334,10 @@ function CategoryItem(props) {
         && Array.isArray(props.item.item_list)
         && props.item.item_list.length > 0
         && props.item.item_list.map(menuItem =>
-        <div key={menuItem.id}>
-          <MenuItem item={menuItem} this={props.this} />
-        </div>
-      )}
+          <div key={menuItem.id}>
+            <MenuItem item={menuItem} this={props.this} />
+          </div>
+        )}
     </div>
   )
 };
@@ -322,12 +347,12 @@ function MenuItem(props) {
     && props.item.item_type.toString()
     && props.item.item_type.toLowerCase() === "non_veg" ? "red" : "green";
   return (
-    <div style={{display:"flex", flexDirection:"row", width:"100%", paddingLeft:"1%"}}>
-      <div style={{width:"5%", display:"flex", alignItems:"center", color: color}}><i className="fa fa-circle" ></i></div>
-      <div style={{width:"65%", display:"flex", alignItems:"center", textTransform:"capitalize"}}> {props.item.item_name} </div>
-      <div style={{width:"20%", display:"flex", alignItems:"center"}}><i className="fa fa-inr" aria-hidden="true"> {props.item.price.toFixed(2)} </i></div>
-      <div style={{width:"10%", display:"flex", alignItems:"center"}}>
-        <IconButton onClick={(e) => props.this.addItemHandler(props.item)}><Add style={{height:"100%"}} /></IconButton>
+    <div style={{ display: "flex", flexDirection: "row", width: "100%", paddingLeft: "1%" }}>
+      <div style={{ width: "5%", display: "flex", alignItems: "center", color: color }}><i className="fa fa-circle" ></i></div>
+      <div style={{ width: "65%", display: "flex", alignItems: "center", textTransform: "capitalize" }}> {props.item.item_name} </div>
+      <div style={{ width: "20%", display: "flex", alignItems: "center" }}><i className="fa fa-inr" aria-hidden="true"> {props.item.price.toFixed(2)} </i></div>
+      <div style={{ width: "10%", display: "flex", alignItems: "center" }}>
+        <IconButton onClick={(e) => props.this.addItemHandler(props.item)}><Add style={{ height: "100%" }} /></IconButton>
       </div>
     </div>
   )
